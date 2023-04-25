68bbcfd42d1c2bf707b181515cda8424b6430610
monikachn <monika.chn@gmail.com>
2023-04-25 16:27:41 +0100

    homework/project-2-com-tutorialninja-sw-3

---
+++ src/utilities/Utility.java
@@ -0,0 +1,60 @@
+package utilities;
+
+import basetest.BaseTest;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+
+public class Utility extends BaseTest {
+    public void clickOnElement(By by) {
+        driver.findElement(by).click();
+    }
+
+    // this method will send text to element
+    public void sendTextToElement(By by, String text) {
+        driver.findElement(by).sendKeys(text);
+    }
+
+    // this method will get text from element
+    public String getTextFromElement(By by) {
+        return driver.findElement(by).getText();
+    }
+
+    public void selectByVisibleTextFromDropDown(By by, String text) {
+        WebElement dropDown = driver.findElement(by);
+        Select select = new Select(dropDown);
+        // Select by visible text
+        select.selectByVisibleText(text);
+    }
+
+    public void selectByValueFromDropDown(By by, String text) {
+        WebElement dropDown = driver.findElement(by);
+        Select select = new Select(dropDown);
+        select.selectByValue(text);
+    }
+
+    public void selectByIndexFromDropDown(By by, int index) {
+        WebElement dropDown = driver.findElement(by);
+        Select select = new Select(dropDown);
+        select.selectByIndex(index);
+    }
+
+    public void mouseHoverToElement(By by) {
+        Actions actions = new Actions(driver);
+        actions.moveToElement(driver.findElement(by)).build().perform();
+    }
+
+    public void mouseHoverToElementAndClick(By by) {
+        Actions actions = new Actions(driver);
+        actions.moveToElement(driver.findElement(by)).click().build().perform();
+    }
+
+    public void verifyFromElement(By by, String text) {
+        String expectedMessage = text;
+        WebElement actualElement = driver.findElement(by);
+        String actualMessage = actualElement.getText();
+        Assert.assertEquals(expectedMessage, actualMessage);
+    }
+}
